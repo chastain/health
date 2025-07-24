0667d0c68d7a36dfb8506c197067e534944e8520
chastain <eric@chastain.me>
2025-07-24 08:45:05 -0400

    updating health and config

--- health.py
+++ health.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# URL Health Tester
-# This code checks http status codes and md5 checksums of page contents.
+# URL Health Monitor
+# This code checks http status codes and md5 checksums of URLs and displays a basic report in the terminal.
 
 import urllib.request
 import urllib.error
@@ -15,10 +15,13 @@ with open('config.json', 'r') as file:
     json_config_data = json.load(file)
 
 # Clear screen and re-run this script every N seconds.
-refresh_after_seconds = json_config_data["refresh_after_seconds"]
+refresh_seconds = json_config_data["refresh_seconds"]
 # How long before we time out a request
-timeout_in_seconds = json_config_data["timeout_in_seconds"]
+timeout_seconds = json_config_data["timeout_seconds"]
 
+# This won't work in unsupported terminal emulators, maybe I should make a wrapper for printing output
+# that can apply these or not based on config or actually do the work to determine if output is on a tty that supports
+# these escape sequences.
 class colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -38,11 +41,12 @@ class colors:
 data = json_config_data["data"]
 
 def verify(record):
+    http_status_code = 0
     try:
         if len(record) == 1:
-            return "heading"
+            return None, "heading"
 
-        response = urllib.request.urlopen(record[1], None, timeout_in_seconds)
+        response = urllib.request.urlopen(record[1], None, timeout_seconds)
         http_status_code = response.getcode()
         compare_md5 = len(record) > 2
 
@@ -50,19 +54,30 @@ def verify(record):
             if compare_md5:
                 md5 = hashlib.md5(response.read()).hexdigest()
                 if md5 != record[2]:
-                    return("md5 mismatch: " + md5) # pass the expected md5 back as our result so we can display it
+                    return http_status_code, "md5 mismatch: " + md5 # pass the expected md5 back as our result so we can display it
 
-            return "success"
+            return http_status_code, "success"
         else:
-            return "warn"
+            return http_status_code, "warn"
 
     except urllib.error.HTTPError as err:
+
+        # I'm treating "Forbidden" responses as warnings.
+        if err.code == 403:
+            return 403, "warn"
+
         # I'm not treating redirects as errors
         if err.code >= 300 and err.code <= 399:
-            return "redirection"
+            return http_status_code, "redirection"
 
-        return "error"
-
+        return err.code, "error"
+    except Exception as ex:
+        # In case the error was in the library we should respond appropriately
+        #return None, "error"
+        #template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        #message = template.format(type(ex).__name__, ex.args)
+        #print(message)
+        return "???", "error"
 
 if __name__ == "__main__":
 
@@ -71,13 +86,12 @@ if __name__ == "__main__":
 
             _ = system("cls") if name == "nt" else system("clear")
 
-            print("\n" + colors.OKBLUE + "URL Health Tester" + colors.ENDC)
-            print(colors.OKBLUE + "(Ctrl+C to exit)" + colors.ENDC + "\n")
-            print("Updated: " + strftime("%Y-%m-%d %H:%M:%S", localtime()) + "\n")
+            print("\n" + colors.OKBLUE + "URL Health Monitor" + colors.ENDC)
+            print("Updated: " + strftime("%Y-%m-%d %H:%M:%S", localtime()) + ". Refreshes every " + str(refresh_seconds) + " seconds.\n")
 
             for id, record in enumerate(data):
 
-                results = verify(record)
+                http_status_code, results = verify(record)
 
                 if results == "heading":
                     color = colors.UNDERLINE + colors.BOLD
@@ -94,11 +108,12 @@ if __name__ == "__main__":
                     n = "\n" if id > 1 else ""
                     print(n + color + record[0] + colors.ENDC + "\n")
                 else:
-                    print(" -> " + color + record[0] + " " + colors.ENDC + colors.GREY + record[1] + colors.ENDC)
+                    print(" -> " + color + record[0] + " [" + str(http_status_code) + "] " + colors.ENDC + colors.GREY + record[1] + colors.ENDC)
                     if results.startswith("md5 mismatch:"):
                         print("     " + results)
 
-            sleep(refresh_after_seconds)
+            print(colors.OKCYAN + "\n(Ctrl+C to exit)" + colors.ENDC + "\n")
+            sleep(refresh_seconds)
 
     except KeyboardInterrupt:
         pass
